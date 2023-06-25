5cd7fa64b08195969df6b9b8634d11cf29e0b1bc
Marcin Andrzejczak <amostori@op.pl>
2023-06-25 17:33:55 +0200

    datasource and repository in data

---
+++ lib/features/show_news/data/datasources/fetch_from_remote_ds.dart
@@ -0,0 +1,35 @@
+import 'package:only_good_news/core/failures_successes/exceptions.dart';
+import 'package:only_good_news/core/services/api_service.dart';
+import 'package:only_good_news/features/show_news/data/models/info_%20news_model.dart';
+import 'package:only_good_news/features/show_news/domain/entities/news_info.dart';
+
+abstract class FetchFromRemoteDatasource {
+  Future<List<NewsInfoModel>> fetchNews(String? searchText);
+}
+
+class FetchFromRemoteDSImpl implements FetchFromRemoteDatasource {
+  ApiService apiService;
+
+  FetchFromRemoteDSImpl({required this.apiService});
+
+  @override
+  Future<List<NewsInfoModel>> fetchNews(String? searchText) async {
+    try {
+      Map<String, dynamic> data = await apiService.getData();
+      List list = data['articles'];
+      List<Map<String, dynamic>> mapList = [];
+      for (int i = 0; i < list.length; i++) {
+        Map<String, dynamic> map = list[i] as Map<String, dynamic>;
+        mapList.add(map);
+      }
+      List<NewsInfoModel> news = [];
+      for (int i = 0; i < mapList.length; i++) {
+        NewsInfoModel newsInfoModel = NewsInfoModel.fromMap(mapList[i]);
+        news.add(newsInfoModel);
+      }
+      return news;
+    } catch (e) {
+      throw FetchException(message: 'Failed to get data.');
+    }
+  }
+}
---
+++ lib/features/show_news/data/repositories/fetch_repo_impl.dart
@@ -0,0 +1,21 @@
+import 'package:dartz/dartz.dart';
+import 'package:only_good_news/core/failures_successes/exceptions.dart';
+import 'package:only_good_news/core/failures_successes/failures.dart';
+import 'package:only_good_news/features/show_news/data/datasources/fetch_from_remote_ds.dart';
+import 'package:only_good_news/features/show_news/domain/entities/news_info.dart';
+import 'package:only_good_news/features/show_news/domain/repositories/fetch_repo_contract.dart';
+
+class FetchRepoImpl implements FetchRepo {
+  final FetchFromRemoteDatasource fetchFromRemoteDatasource;
+
+  FetchRepoImpl({required this.fetchFromRemoteDatasource});
+
+  @override
+  Future<Either<Failure, List<NewsInfo>>> fetchNews(String? searchText) async {
+    try {
+      return right(await fetchFromRemoteDatasource.fetchNews(searchText));
+    } on FetchException catch (e) {
+      return left(FetchFailure(message: e.message));
+    }
+  }
+}
